117219e17760f26234636a213f635d4e1e8a7a31
AM <aikidoesurf81@hotmail.com>
2022-06-21 11:19:34 -0700

    get shaper files

--- routes/blob.js
+++ routes/blob.js
@@ -31,4 +31,25 @@ router.get('/getFiles/:listing_id', function (req, res, next) {
     }
 });
 
+router.get('/getShaperFiles/:shaper_id', function (req, res, next) {
+    console.log('getFiles');
+    let files = [];
+    const containerName = 'shapers'; //+ req.params.listing_id;
+    const listing_id = req.params.shaper_id;
+    
+    getFiles();
+    
+    async function getFiles() {
+        const containerClient = blobServiceClient.getContainerClient(containerName);
+
+        let i = 1;
+        let iter = await containerClient.listBlobsFlat();
+        for await (const blob of iter) {
+            //console.log(`Blob ${i++}: ${blob.name}`);
+            if (blob.name.indexOf(shaper_id + '/') == 0) files.push(blob.name);
+        }
+        res.status(200).send(files);
+    }
+});
+
 module.exports = router;
